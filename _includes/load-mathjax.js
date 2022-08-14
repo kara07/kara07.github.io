2fc8824cf232c642f424a3d84e69c2cd952c922d
kara07 <y00a445gg@mozmail.com>
2022-08-15 02:47:42 +0800

    mathjax

---
+++ _includes/load-mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+    loader: {load: ['[tex]/physics']},
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        packages: {'[+]': ['physics']
+    },
+    svg: {
+        fontCache: 'global'
+    }
+};
+
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/latest?tex-svg.js';
+    script.async = true;
+    document.head.appendChild(script);
+})();
